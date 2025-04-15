4a5e7d0f5985cccab78a20d51d38188c9dcf4bb8
Matthew-Seok <matthew.seok@gmail.com>
2025-04-15 23:02:00 +0900

    select 메서드 테스트

--- src/test.py
+++ src/test.py
@@ -4,14 +4,14 @@ from bs4 import BeautifulSoup
 url = 'https://www.naver.com'
 response = requests.get(url) # url에 get 요청을 보냄
 # print(response) # 200은 성공을 의미
-print(response.text)  # HTML 소스 출력
+# # print(response.text)  # HTML 소스 출력
 
-# 테스트용 html
+# (위의 HTML 소스 대신 테스트용 html을 사용하기 위한 예제)
 html = """
 <nav class="menu-box-1" id="menu-box">
   <ul>
     <li>
-      <a class="naver" href="https://www.naver.com">네이버로 이동</a>
+      <a class="naver a b c d" href="https://www.naver.com">네이버로 이동</a>
     </li>
     <li>
       <a class="google" href="https://www.google.com">구글로 이동</a>
@@ -44,7 +44,7 @@ find_li = soup.find_all('li');
 for idx, li in enumerate(find_li):
   print(f"{idx} : {li}")
 
-# html 상에 있는 모든 a를 검새개서 모두 가져온다.
+# html 상에 있는 모든 a를 검색해서 모두 가져온다.
 print(soup.find_all('a'))
 
 find_a = soup.find_all('a')
@@ -52,3 +52,42 @@ find_a = soup.find_all('a')
 for idx, a in enumerate(find_li):
   a_text = a.get_text().strip()
   print(f"{idx} : {a_text.strip()}")
+
+# ! select_one() 테스트
+# class 이름이 menu-box-1 인 엘리먼트 검색
+print("== select_one 테스트, class 검색 ==")
+print(soup.select_one('.menu-box-1'))
+
+# id 이름이 menu-box-1 인 엘리먼트 검색
+print("== select_one 테스트, id 검색 ==")
+print(soup.select_one('#menu-box'))
+
+# nav > ul > li : nav의 자식의 ul, ul의 자식인 li를 검색
+print("== select_one 테스트, 선택자 검색 ==")
+print(soup.select_one('nav > ul > li'))
+
+# ! select() 테스트
+# 1. select()
+
+# 반복문을 이용하여 데이터 순회
+select_a = soup.select('.menu-box-1 a')
+
+# v1 : 엘리먼트 속성값 가져오기
+print("== 엘리먼트 속성값 가져오기 ==")
+for idx, a in enumerate(select_a):
+  print(a.attrs)  # attrs : 엘리먼트의 속성을 딕셔너리 객체로 변환
+
+# v2 : 엘리먼트 href 속성값 가져오기
+print("== 엘리먼트 href 속성값 가져오기 ==")
+for idx, a in enumerate(select_a):
+  print(a.attrs['href'])  # href 키로 접근하여 속성값 가져오기
+
+# v3 : 엘리먼트 class 속성값 가져오기
+print("== 엘리먼트 class 속성값 가져오기 ==")
+for idx, a in enumerate(select_a):
+  print(a.attrs['class'])  # class 키로 접근하여 속성값 가져오기
+
+# v1 : 엘리먼트의 text 값 가져오기
+print("== 엘리먼트의 text 값 가져오기 ==")
+for idx, a in enumerate(select_a):
+  print(f"{idx} : {a.get_text()}")
